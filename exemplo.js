0a3e5af591a164db1d537c0273568f032f3d99c4
FELIPE NEVES MONTEIRO <42789106819@imaccommand011.local>
2025-11-18 07:05:25 -0800

    Exercicio03

--- exemplo.js
+++ exemplo.js
@@ -10,7 +10,7 @@
 
 // console .log("Multimidia")
 
-// let nome = prompt("Informe o seu nome: ")
+// let nome = prompt("Informe o seu nome: ")git 
 
 // console.log("Bem vindo! " + nome)
 
@@ -18,7 +18,7 @@
 // let = numero2 = Number.parseInt(prompt("Digite o segundo número: "))
 
 // let resultado = numero1 + numero2
-    
+
 // console.log(resultado)
 
 // let = numero1 = Number.parseFloat(prompt("Digite o primeiro número: "))
---
+++ exercicio03.js
@@ -0,0 +1,17 @@
+// Exercício 3: Ímpar ou par
+// Faça um programa que receba um número do usuário e informe se este número é par ou
+// ímpar.
+
+let numero = parseInt(prompt("Informe um número inteiro: "))
+//Transforma o texto em número
+
+if (numero % 2 == 0 ) {
+    console.log("O número par ")
+} else {
+    console.log("O número é impar")
+}
+
+
+
+
+
