ef7350d907f8507256e56bb66176571b18989e2b
FELIPE NEVES MONTEIRO <42789106819@imaccommand011.local>
2025-11-18 09:38:19 -0800

    Exercicio05

--- exercicio05.js
+++ exercicio05.js
@@ -4,9 +4,9 @@
 // não.
 
 
-let n1 = Number(prompt("Digite o primeiro número: "));
-let n2 = Number(prompt("Digite o segundo número: "));
-let n3 = Number(prompt("Digite o terceiro número: "));
+let n1 = parseInt(prompt("Digite o primeiro número: "));
+let n2 = parseInt(prompt("Digite o segundo número: "));
+let n3 = parseInt(prompt("Digite o terceiro número: "));
 
 if (n1 < n2 && n2 < n3) {
     console.log("Os números estão em ordem crescente!");
